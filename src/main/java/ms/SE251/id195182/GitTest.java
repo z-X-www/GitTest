9d82d19d45bbe7583b6def83765b98aa33d34e63
ZhaoXW <957176527@qq.com>
2021-09-13 08:50:06 +0800

    second commit

--- src/main/java/ms/SE251/id195182/GitTest.java
+++ src/main/java/ms/SE251/id195182/GitTest.java
@@ -3,5 +3,6 @@ package ms.SE251.id195182;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git");
+        System.out.println("Hello Git2");
     }
 }
